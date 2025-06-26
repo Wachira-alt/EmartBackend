2fd1de44762b6700d124cc97858fa778f8c32c01
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-06-26 11:16:03 +0300

    added cart items

--- models/cart_item.py
+++ models/cart_item.py
@@ -14,7 +14,7 @@ class CartItem(db.Model, SerializerMixin):
     user = db.relationship("User", back_populates="cart_items")
     product = db.relationship("Product", back_populates="cart_items")
 
-    # serialize_rules = ('-user.cart_items', '-product.cart_items' ...,)
+    # serialize_rules = ('-user.cart_items', '-product.cart_items' ....,)
     serialize_rules = ('-user', 'product',)
 
 
