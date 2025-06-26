c3fe58203d7d2515cd0d4bb63be80bf02d105315
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-06-26 11:03:48 +0300

    added cart-items

--- models/cart_item.py
+++ models/cart_item.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 class CartItem(db.Model, SerializerMixin):
+    #
     __tablename__ = 'cart_items'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,7 +14,7 @@ class CartItem(db.Model, SerializerMixin):
     user = db.relationship("User", back_populates="cart_items")
     product = db.relationship("Product", back_populates="cart_items")
 
-    # serialize_rules = ('-user.cart_items', '-product.cart_items',)
+    # serialize_rules = ('-user.cart_items', '-product.cart_items' ...,)
     serialize_rules = ('-user', 'product',)
 
 
